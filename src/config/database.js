3ce7563c9cebf69b336c2b8e3dc4c0a19beecf18
WandersonFullStack <wandersonmag.silva@gmail.com>
2026-02-28 01:13:14 -0300

    Configurando o Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'locahost',
+    port: 5432,
+    username: 'barney',
+    password: 'admin762',
+    database: 'dev-burguer-db',
+    define: {
+        timestamps: true,
+        uderscored: true,
+        uderscoredAll: true,
+    },
+};
