ff60ce9dc6bce1f230f977023215d49c43e570e2
WandersonFullStack <wandersonmag.silva@gmail.com>
2026-03-03 15:48:45 -0300

    Criando controller de users

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from "express";
-import User from "./app/models/User.js";
-import { v4 } from 'uuid';
+
+import UserController from "./app/controllers/UserController.js";
 
 const routes = new Router();
 
-routes.get('/', async ( req, res) => {
-    const user = {
-        id: v4(),
-        name: 'Wanderson',
-        email: 'wanderson@gmail.com',
-        password_hash: 'admin1223',
-        admin: false,
-    }
-
-    await User.create(user);
-
-    res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
