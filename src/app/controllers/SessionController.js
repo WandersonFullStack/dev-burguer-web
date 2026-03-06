f0671d24154878587ad3c33d9ccaa3e61fcfea11
WandersonFullStack <wandersonmag.silva@gmail.com>
2026-03-06 01:35:10 -0300

    Criando controller de login

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,51 @@
+import * as Yup from 'yup';
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+
+class SessionController {
+    async store(request, response) {
+        const schema = Yup.object({
+            email: Yup.string().email().required(),
+            password: Yup.string().min(8).required(),
+        });
+
+        const dataIncorrect = () => {
+            return response.status(400).json({ error: 'E-mail or password incorrect' });
+        }
+        
+        const isValid = await schema.isValid(request.body, { strict: true });
+        
+        if(!isValid) {
+            dataIncorrect();
+        }
+        
+        const { email, password } = request.body;
+
+        const existinUser = await User.findOne(
+            {
+                where: {
+                    email
+                }
+            }
+        );
+
+        if (!existinUser) {
+            dataIncorrect();
+        }
+        
+        const isPasswordCorrect = await bcrypt.compare(password, existinUser.password_hash);
+
+        if (!isPasswordCorrect) {
+            dataIncorrect();
+        }
+
+        return response.status(200).json({ 
+            id: existinUser.id,
+            name: existinUser.name,
+            email: existinUser.email,
+            admin: existinUser.admin,
+         });
+    }
+}
+
+export default new SessionController();
